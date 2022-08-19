09ece9869e5828f7e78aa77e9a9ff4c931425e7e
cotabas <cotabas@gmail.com>
2022-08-18 22:08:05 -0700

    first section: done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,19 @@ class UsersController < ApplicationController
 
   def create
 
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_users_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :email)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "user#index"
+#  root "user#index"
 
   resource :users, only: [:new, :create]
   

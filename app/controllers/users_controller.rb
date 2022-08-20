30ba25475dbbe2e9161a099d13672087c2900bad
cotabas <cotabas@gmail.com>
2022-08-19 22:32:01 -0700

    next piece also done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
   def new
-
+    @user = User.new
   end
 
   def create
 
-#    @user = User.new(user_params)
-    @user = User.new(username: params[:username], password: params[:password], email: params[:email])
+    @user = User.new(user_params)
+#   @user = User.new(username: params[:username], password: params[:password], email: params[:email])
 
     if @user.save
       redirect_to new_users_path
